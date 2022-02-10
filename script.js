e68c723033114cc5a29b4d0ba6d5e9abb3ed40bf
ElenaGir <elena.girard@gmail.com>
2022-02-10 15:58:11 +0100

    js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let myForm = document.getElementById('myForm');
+
+let mail = document.querySelector(".mail");
+let message = document.querySelector(".message");
+
+
 myForm.addEventListener('submit' function(e){
+    let name = document.querySelector(".name");
+    
 
 });
