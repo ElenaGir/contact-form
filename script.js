263c4d1d02ff84165fc9d5af5fa0b49a68879278
ElenaGir <elena.girard@gmail.com>
2022-02-11 15:33:15 +0100

    js form

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-// let myForm = document.getElementById('myForm');
-// let mail = document.querySelector(".mail");
-// let myerror1 = getElementById("error1");
-// let message = document.querySelector(".message");
-// let myerror2 = getElementById("error2");
-// let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let myForm = document.getElementById('myForm');
+let mail = document.querySelector(".mail");
+let myerror1 = getElementById("error1");
+let message = document.querySelector(".message");
+let myerror2 = getElementById("error2");
+let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
-// myForm.addEventListener('submit'), function(e) {
-//     let name = document.querySelector(".name");
+myForm.addEventListener('submit'), function(e) 
+
+
+// let name = document.querySelector(".name");
 //     let nameRegex = /^[a-zA-Z-\s]$/;
-   
-//     if (name.value == "") {
-//         let myerror = getElementById("error");
-//         myerror.InnerHTML = "Please enter a valid name (e.g. John Smit)";
-//         myerror.style.color = "red";
-//         name.style.color = "red";
-//         e.preventDefault();
+// if (name.value == "") {
+//     let myerror = getElementById("error");
+//     myerror.InnerHTML = "Please enter a valid name (e.g. John Smit)";
+//     myerror.style.color = "red";
+//     name.style.color = "red";
+//     e.preventDefault();
 
-//     } else if (nameRegex.test(name.value) == false){
-//         let myerror = getElementById("error");
-//         myerror.InnerHTML = "Please enter a valid name (e.g. John Smit)";
-//         myerror.style.color = "red";
-//         name.style.color = "red";
-//         e.preventDefault();
-//     }
-// });
+// } else if (nameRegex.test(name.value) == false){
+//     let myerror = getElementById("error");
+//     myerror.InnerHTML = "Please enter a valid name (e.g. John Smit)";
+//     myerror.style.color = "red";
+//     name.style.color = "red";
+//     e.preventDefault();
+// }
