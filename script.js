ad978e05c4d3ef627d62620611f512e9c8be8ccb
ElenaGir <elena.girard@gmail.com>
2022-02-14 17:29:15 +0100

    add favicon+php

--- script.js
+++ script.js
@@ -1,27 +1,73 @@
+let Name = document.getElementsByClassName('name')[0];
+let myerror = document.getElementById("error");
 let myForm = document.getElementById('myForm');
-let mail = document.querySelector(".mail");
-let myerror1 = getElementById("error1");
-let message = document.querySelector(".message");
-let myerror2 = getElementById("error2");
+const mail = document.getElementsByClassName("mail")[0];
+const myerror1 = document.getElementById("error1");
+const Message = document.getElementsByClassName('message')[0];;
+const myerror2 = document.getElementById("error2");
 let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const button = document.getElementsByClassName('submit')[0];
+
+button.addEventListener('click', () => {
+    let Mail = mail.value;
+    let mailvalid = Mail.match(emailRegex);
+    let name = Name.value;
+    let message = Message.value;
+
+    if (Mail == "") {
+        
+    myerror1.textContent = "Please enter a valid email (e.g. johnsmit@mail.com)"; 
+    myerror1.style.color = "red";
+    mail.style.border= '2px solid red';
+    mail.style.borderTop= 'none';
+    mail.style.borderRight= 'none';
+    mail.style.borderLeft= 'none';
 
 
-myForm.addEventListener('submit'), function(e) 
+    } else if (mailvalid == Mail.match(emailRegex)){
+            myerror1.textContent = "Please enter a valid email (e.g. johnsmit@mail.com)"; 
+            myerror1.style.color = "red";
+            mail.style.border= '2px solid red';
+            mail.style.borderTop= 'none';
+            mail.style.borderRight= 'none';
+            mail.style.borderLeft= 'none';
+        }
+            if (name == ""){
+                myerror.textContent = "Please enter a valid name (e.g. John Smit)"; 
+                myerror.style.color = "red";
+                Name.style.border= '2px solid red';
+                Name.style.borderTop= 'none';
+                Name.style.borderRight= 'none';
+                Name.style.borderLeft= 'none';
+            }
+
+                if (message == ""){
+                    myerror2.textContent = "Please enter a message"; 
+                    myerror2.style.color = "red";
+                    Message.style.border= '2px solid red';
+                    Message.style.borderTop= 'none';
+                    Message.style.borderRight= 'none';
+                    Message.style.borderLeft= 'none';
+
+    }
+    
+    // myForm.addEventListener("submit", (e){
+
+    // }
+    
+    });
 
 
-// let name = document.querySelector(".name");
-//     let nameRegex = /^[a-zA-Z-\s]$/;
-// if (name.value == "") {
-//     let myerror = getElementById("error");
-//     myerror.InnerHTML = "Please enter a valid name (e.g. John Smit)";
-//     myerror.style.color = "red";
-//     name.style.color = "red";
-//     e.preventDefault();
+    
 
-// } else if (nameRegex.test(name.value) == false){
-//     let myerror = getElementById("error");
-//     myerror.InnerHTML = "Please enter a valid name (e.g. John Smit)";
-//     myerror.style.color = "red";
-//     name.style.color = "red";
-//     e.preventDefault();
-// }
+
+
+
+
+
+   
+   
+
+
+
+
--- traitement.php
+++ traitement.php
@@ -1,2 +1,44 @@
 <?php
-echo 'traitement';
+/* Page: contact.php */
+//mettez ici votre adresse mail
+$VotreAdresseMail="elena.girard@gmail.com";
+// si le bouton "SUBMIT" est cliqué
+if(isset($_POST['submit'])) {
+    //on vérifie que le champ mail est correctement rempli
+    if(empty($_POST['mail'])) {
+        echo "The email field is empty";
+    } else {
+        //on vérifie que l'adresse est correcte
+        if(!preg_match("#^[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?@[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?\.[a-z]{2,}$#i",$_POST['mail'])){
+            echo "The email address is incorrect";
+        }else{
+            //on vérifie que le champ sujet est correctement rempli
+            if(empty($_POST['name'])) {
+                echo "The name field is empty";
+            }else{
+                //on vérifie que le champ message n'est pas vide
+                if(empty($_POST['message'])) {
+                    echo "The message field is empty";
+                }else{
+                    //tout est correctement renseigné, on envoi le mail
+                    //on renseigne les entêtes de la fonction mail de PHP
+                    $Entetes = "MIME-Version: 1.0\r\n";
+                    $Entetes .= "Content-type: text/html; charset=UTF-8\r\n";
+                    $Entetes .= "From: Formulaire <".$_POST['mail'].">\r\n";//de préférence une adresse avec le même domaine de là où, vous utilisez ce code, cela permet un envoie quasi certain jusqu'au destinataire
+                    $Entetes .= "Reply-To: Formulaire <".$_POST['mail'].">\r\n";
+                    //on prépare les champs:
+                    $Mail=$_POST['mail']; 
+                    $Sujet='=?UTF-8?B?'.base64_encode($_POST['nom']).'?=';//Cet encodage (base64_encode) est fait pour permettre aux informations binaires d'être manipulées par les systèmes qui ne gèrent pas correctement les 8 bits (=?UTF-8?B? est une norme afin de transmettre correctement les caractères de la chaine)
+                    $Message=htmlentities($_POST['msg'],ENT_QUOTES,"UTF-8");//htmlentities() converti tous les accents en entités HTML, ENT_QUOTES Convertit en + les guillemets doubles et les guillemets simples, en entités HTML
+                    //en fin, on envoi le mail
+                    if(mail($VotreAdresseMail,$Sujet,nl2br($Message),$Entetes)){//la fonction nl2br permet de conserver les sauts de ligne et la fonction base64_encode de conserver les accents dans le titre
+                        echo "THANK YOU";
+                    } else {
+                        echo "Sorry, there was a problem sending your message";
+                    }
+                }
+            }
+        }
+    }
+}
+?>
